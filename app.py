ed5ad97896c4e45af9f20fe900887ecf4baa50e0
XiayiZhang <122589407+XiayiZhang@users.noreply.github.com>
2024-02-02 21:09:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,3 @@ def wol():
      flash(message)
      if __name__ == '__main__':
         app.run(debug=True)
-~                                                                                                                                
-~                                                                                                                                
-~                                 
